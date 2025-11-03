29ec8c4c0f8f2792682f3dbca811a54af33b88d4
DangTaiNguyen2004 <153304870+DangTaiNguyen2004@users.noreply.github.com>
2025-11-03 20:47:33 +0700

    Add files via upload

---
+++ day10/data/categories.js
@@ -0,0 +1,5 @@
+const categories = [
+  { id: 1, name: "Phòng khách" },
+  { id: 2, name: "Phòng bếp" },
+  { id: 3, name: "Phòng ngủ" }
+];
---
+++ day10/data/products.js
@@ -0,0 +1,16 @@
+const products = [
+  // === PHÒNG KHÁCH ===
+  { id: 1, name: "Bàn uống nước", oldPrice: 8500000, price: 8390000, image: "images/1.png", categoryId: 1 },
+  { id: 2, name: "Kệ TV", oldPrice: 4800000, price: 4560000, image: "images/1.png", categoryId: 1 },
+  { id: 3, name: "Kệ để đồ", oldPrice: 6000000, price: 4930000, image: "images/1.png", categoryId: 1 },
+  { id: 4, name: "SOFA", oldPrice: 10000000, price: 8400000, image: "images/1.png", categoryId: 1 },
+
+  // === PHÒNG BẾP ===
+  { id: 5, name: "Bàn ăn", oldPrice: 11000000, price: 10550000, image: "images/1.png", categoryId: 2 },
+  { id: 6, name: "Bàn trà", oldPrice: 4000000, price: 1600000, image: "images/1.png", categoryId: 2 },
+
+  // === PHÒNG NGỦ ===
+  { id: 7, name: "Giường gỗ", oldPrice: 4200000, price: 4000000, image: "images/1.png", categoryId: 3 },
+  { id: 8, name: "Tủ quần áo", oldPrice: 34000000, price: 11300000, image: "images/1.png", categoryId: 3 },
+  { id: 9, name: "Giường châu Âu", oldPrice: 12000000, price: 8999000, image: "images/1.png", categoryId: 3 }
+];
