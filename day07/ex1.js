69f773c3ca9c63b46c7173a0f29ad1d206c09d89
DangTaiNguyen2004 <153304870+DangTaiNguyen2004@users.noreply.github.com>
2025-11-03 19:53:41 +0700

    Add files via upload

---
+++ day07/ex1.js
@@ -0,0 +1,16 @@
+var name = "Tai Nguyen";
+console.log("Welcome to,", name);
+
+var age1 = 50
+
+console.log("Welcome to,", name, "Tuổi:", age1);
+
+
+let age = 20;
+var name = "Nguyen";
+const PI = 3.14159;
+console.log(age, name, PI);
+
+// let age = 100; => error 
+age = 100;
+console.log(age, name, PI);
